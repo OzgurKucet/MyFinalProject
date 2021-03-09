d0b363f0c306475a9cc9ca7f0e33eaf76e2f4c8d
Ozgur Kucet <ozgurkucet@hotmail.com>
2021-03-09 16:59:44 +0300

    update

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -15,10 +15,10 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product> { 
                 new Product{ProductId = 1,CategoryId=1,ProductName="Bardak",UnitPrice = 15,UnitsInStock = 15},
-                new Product{ProductId = 1,CategoryId=1,ProductName="Kamera",UnitPrice = 500,UnitsInStock = 3},
-                new Product{ProductId = 1,CategoryId=1,ProductName="Telefon",UnitPrice = 1500,UnitsInStock = 2},
-                new Product{ProductId = 1,CategoryId=1,ProductName="Klavye",UnitPrice = 150,UnitsInStock = 65},
-                new Product{ProductId = 1,CategoryId=1,ProductName="Fare",UnitPrice = 85,UnitsInStock = 1}
+                new Product{ProductId = 2,CategoryId=1,ProductName="Kamera",UnitPrice = 500,UnitsInStock = 3},
+                new Product{ProductId = 3,CategoryId=1,ProductName="Telefon",UnitPrice = 1500,UnitsInStock = 2},
+                new Product{ProductId = 4,CategoryId=1,ProductName="Klavye",UnitPrice = 150,UnitsInStock = 65},
+                new Product{ProductId = 5,CategoryId=1,ProductName="Fare",UnitPrice = 85,UnitsInStock = 1}
 
             };
         }
